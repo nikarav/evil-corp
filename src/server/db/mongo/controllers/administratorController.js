22d713b7c944f7fcd94f9002313c884e87245fe6
vasilikiv <el13015@central.ntua.gr>
2018-02-19 19:09:36 +0200

    added checkIfLocked and lockUnlockUser functions for administrator

--- src/server/db/mongo/controllers/administratorController.js
+++ src/server/db/mongo/controllers/administratorController.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 import User from '../models/user_model';
 import AdministratorProfile from '../models/administrator_model';
+//import UserProfile from '../models/user_model';
+import ParentProfile from '../models/parent_model';
+import ProviderProfile from '../models/provider_model';
+import Activity from '../models/activity_model';
 import { USER_TYPES } from '../../../../config/userTypes';
 import validator from 'validator';
 /**
@@ -73,9 +77,89 @@ export function administratorData(req, res) {
   return res.send(data);
 }
 
+export function lockUnlockUser(req, res, next){
+    const username = req.body.username;
+    User.findOne({ username: username }, (findErr, existingUser) => {
+      if (!existingUser) {
+        return res.sendStatus(409);
+      }
+      const userProfile = existingUser;
+      const role = userProfile.user_role;
+      const profileId = userProfile.profile;
+      const parent = role === USER_TYPES.Parent;
+      const provider = role === USER_TYPES.Provider;
+      if(parent) {
+        ParentProfile.findById(profileId, (err, profile) => {
+          if (err) return next(err);
+          const lockedUpdated = !profile.locked;
+          ParentProfile.findByIdAndUpdate(profileId, { $set: { "locked": lockedUpdated } }, { new: true}, (err, profile) => {
+            if (err) return next(err);
+          });
+        return res.send({ locked: lockedUpdated });
+        });
+     }
+     else if (provider) {
+       ProviderProfile.findById(profileId, (err, profile) => {
+         if (err) return next(err);
+         const lockedUpdated = !profile.locked;
+         ProviderProfile.findByIdAndUpdate(profileId, { $set: { "locked": lockedUpdated } }, { new: true}, (err, profile) => {
+           if (err) return next(err);
+         });
+         const activities = profile.activities;
+         const arrayLength = activities.length;
+         for (var i = 0; i < arrayLength; i++) {
+            var activityId = activities[i];
+            Activity.findByIdAndUpdate(activityId,  { $set: { "locked": lockedUpdated } }, { new: true}, (err, profile) => {
+              if (err) return next(err);
+            });
+         }
+       return res.send({ locked: lockedUpdated });
+       });
+     }
+     else{
+       const response = "Username does not correspond to parent/provider"
+       return res.send({ response });
+     }
+    });
+}
+
+export function checkIfLocked(req, res, next){
+    const username = req.body.username;
+    User.findOne({ username: username }, (findErr, existingUser) => {
+      if (!existingUser) {
+        return res.sendStatus(409);
+      }
+      const userProfile = existingUser;
+      const role = userProfile.user_role;
+      const profileId = userProfile.profile;
+      const parent = role === USER_TYPES.Parent;
+      const provider = role === USER_TYPES.Provider;
+      if(parent) {
+        ParentProfile.findById(profileId, (err, profile) => {
+          if (err) return next(err);
+          const locked = profile.locked;
+          return res.send({ locked: locked });
+        });
+     }
+     else if (provider) {
+       ProviderProfile.findById(profileId, (err, profile) => {
+         if (err) return next(err);
+         const locked = profile.locked;
+         return res.send({ locked: locked });
+       });
+     }
+     else{
+       const response = "Username does not correspond to parent/provider"
+       return res.send({ response });
+     }
+    });
+}
+
 export default {
   administratorSignup,
   authorizeAdministrator,
   changeEmail,
-  administratorData
+  administratorData,
+  lockUnlockUser,
+  checkIfLocked
 };
