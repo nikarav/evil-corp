b27fa2ff934c27ba7af940740a85ea8d3e376c12
Dimitrios Sotiriou <el12059@central.ntua.gr>
2018-02-19 21:26:05 +0200

    Locked users are not allowed to login

--- src/server/db/mongo/passport/local.js
+++ src/server/db/mongo/passport/local.js
@@ -1,8 +1,13 @@
 import User from '../models/user_model';
+import { USER_TYPES } from '../../../../config/userTypes';
 
 export default (req, username, password, done) => {
   User.findOne({ username }).populate('profile').exec((findErr, user) => {
     if (!user) return done(null, false, { message: `There is no record of the username ${username}.` });
+    const isAdmin = user.profile.user_role === USER_TYPES.Administrator;
+    if (!isAdmin && user.profile.locked === true) {
+      return done(null, false, { message: 'Your account is locked.' });
+    }
     return user.comparePassword(password, (passErr, isMatch) => {
       if (isMatch) {
         return done(null, user);
