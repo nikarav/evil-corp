b3ba25aad78d4c80b393284991232a1357fb00e9
Manolis <manolisan28@gmail.com>
2018-02-22 21:33:38 +0200

    Defined parent related requests in parentService.js

--- src/app/services/parentService.js
+++ src/app/services/parentService.js
@@ -6,13 +6,37 @@ export default () => {
   return {
     getCredits: () => client.request({
       method: 'GET',
-      url: '/api/parents/credits',
+      url: '/api/parent/credits',
     }),
     addCredits: ( {credits}) => client.request({
       method: 'POST',
-      url: 'api/parents/credits',
+      url: '/api/parent/credits',
       data: {
         credits
+      }
+    }),
+    getParentData: ({user}) => client.request({
+      method: 'GET',
+      url: '/api/parent',
+    }),
+    changeProfile: ({ name, surname, email, telephone, address, birthday }) => client.request({
+      method: 'POST',
+      url: '/api/parent/changeProfile',
+      data: {
+        name,
+        surname,
+        email,
+        telephone,
+        address,
+        birthday
+      }
+    }),
+    changeCredentials: ({ username, password }) => client.request({
+      method: 'POST',
+      url: '/api/parent/changeCredentials',
+      data: {
+        username,
+        password
       }
     }),
   };
