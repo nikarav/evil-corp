879a8c18fc94b4c7f2dea1eadb7e33850079bb12
Dimitrios Sotiriou <el12059@central.ntua.gr>
2018-02-23 22:15:01 +0200

    Added https support for production environment

--- src/config/app.js
+++ src/config/app.js
@@ -4,8 +4,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-// TODO Change apiEndpoint for production
-export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'http://localhost:3000';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://localhost:3000';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import webpack from 'webpack';
+import https from 'https';
+import fs from 'fs';
 import { isDebug } from '../config/app';
 import { connect } from './db';
 import initPassport from './init/passport';
 import initExpress from './init/express';
 import initRoutes from './init/routes';
+import { ENV } from '../config/env';
 
 import renderMiddleware from './render/middleware';
 
@@ -42,6 +45,15 @@ initExpress(app);
  */
 initRoutes(app);
 
+/* HTTPS Support */
+const privateKey = fs.readFileSync('server/sslcert/key.key', 'utf8');
+const certificate = fs.readFileSync('server/sslcert/key.crt', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate
+};
+
 /*
  * This is where the magic happens. We take the locals data we have already
  * fetched and seed our stores with data.
@@ -50,4 +62,8 @@ initRoutes(app);
  */
 app.get('*', renderMiddleware);
 
-app.listen(app.get('port'));
+if (ENV === 'production') {
+  https.createServer(credentials, app).listen(app.get('port'));
+} else {
+  app.listen(app.get('port'));
+}
