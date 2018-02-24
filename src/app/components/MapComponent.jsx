b38ccb29753d28900d48ea52cc1be8aba68faf19
Dimitris <jimadamis95@yahoo.com>
2018-02-24 21:05:07 +0200

    Google map with clustering /map

--- src/app/components/MapComponent.jsx
+++ src/app/components/MapComponent.jsx
@@ -1,21 +1,119 @@
 import React from 'react';
 import { Block } from 'jsxstyle';
+import { withScriptjs,withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+
+import { compose, withProps,withHandlers, withStateHandlers } from  "recompose"
+const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
+
+
+const FaAnchor = require("react-icons/lib/fa/anchor");
+
+
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
+
+
+
 
 
 class MapComponent extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: null,
+    }
+
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+    this.onMarkerClustererClick = this.onMarkerClustererClick.bind(this);
+  }
+
+
+  onMarkerClustererClick (markerClusterer) {
+    const clickedMarkers = markerClusterer.getMarkers()
+    console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+    console.log(clickedMarkers)
+  }
+
+  //
+  onMarkerClick(id) {
+    console.log('onMarkerClick');
+    this.setState({
+      activeMarker: id,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked(props) {
+    console.log('onMapClick');
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
+
+
+  render(){
+
+    const CustomMarker = (props) => {
+      const {id} = props;
+
+      const onMarkerClick = (evt) => {
+        console.log(id);
+         this.onMarkerClick(id);
+      }
 
       return (
-          <Block>
-
-            <h1>
-              put map here
-            </h1>
-          </Block>
+        <Marker
+        onClick={onMarkerClick}
+        {...props}
+        />
       );
+    };
+
+
+    return(
+      <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 37.9838, lng: 23.7275 }}
+      onClick ={this.onMapClicked}
+      >
+        <MarkerClusterer
+        onClick={this.onMarkerClustererClick}
+        averageCenter
+        enableRetinaIcons
+        gridSize={60}
+        >
+          {!this.props.markers?null:
+            this.props.markers.map((marker, index) => (
+              <CustomMarker
+                key={index}
+                id={index}
+                position={{ lat: marker.lat, lng: marker.lng }}
+
+              >
+              {this.state.showingInfoWindow && this.state.activeMarker == index &&
+              <InfoWindow>
+                  <FaAnchor />
+              </InfoWindow>}
+              }
+              </CustomMarker>
+              )
+            )
+          }
+
+
+          </MarkerClusterer>
+      </GoogleMap>
+    );
   }
 }
 
-export default MapComponent;
+
+export default withScriptjs(withGoogleMap(MapComponent));
